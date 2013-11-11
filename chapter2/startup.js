3cda37fc0d6d9032c4aadc4fbfb830029d4664a0
robertlyc <merlinlyc@gmail.com>
2013-11-11 13:23:23 +0800

    add ng-submit

--- chapter2/startup.js
+++ chapter2/startup.js
@@ -1,10 +1,11 @@
 function StartUpController ($scope) {
   $scope.funding = { startingEstimate: 0 };
   
-  computeNeeded = function() {
+  $scope.computeNeeded = function() {
     $scope.funding.needed = $scope.funding.startingEstimate * 10;
   }
-  
-  $scope.$watch('funding.startingEstimate', computeNeeded);
-  
+    
+  $scope.requestFunding = function() {
+    window.alert('Sorry, please get more customers first.');
+  }
 }
